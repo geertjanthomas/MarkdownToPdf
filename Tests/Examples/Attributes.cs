dcc082245f39e51ce6d4552e92be6c5aecc0e736
Thomas, G (Geert-Jan) <Geert-Jan.Thomas@rabobank.com>
2025-12-19 08:45:19 +0100

    Improved versioning

--- Tests/Examples/Attributes.cs
+++ Tests/Examples/Attributes.cs
@@ -17,9 +17,15 @@ public static class Attributes
 
         pdf.WarningIssued += (o, e) => { Console.WriteLine($"{e.Category}: {e.Message}"); };
 
+        var fontstyle = pdf.StyleManager.AddStyle("myFont", MarkdownStyleNames.InlineCode);
+        fontstyle.Font.Name = "Courier New";
+        fontstyle.Font.Bold = true;
+        //fontstyle.Font.Color = Color.Red;
+
         var style = pdf.StyleManager.AddStyle("myList", MarkdownStyleNames.UnorderedListItem);
         style.Bullet.Normal.Content = "*";
         pdf.StyleManager.ForElement(ElementType.UnorderedListItem).WithParent(ElementType.UnorderedList, "myList").Bind(style);
+        pdf.StyleManager.ForElement(ElementType.InlineCode).Bind(fontstyle);
         pdf
          .Add(markdown)
          .Save("attributes.pdf");
